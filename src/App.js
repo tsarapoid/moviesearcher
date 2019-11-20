b43ee0a00a8258cb7ecd7c623d83f325fe393191
tsarapoid <nadein@gmail.com>
2019-11-20 23:05:32 +0300

    visual styles added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import TextField from '@material-ui/core/TextField'
+import { withStyles } from '@material-ui/core/styles'
+import Card from '@material-ui/core/Card'
+import CardActionArea from '@material-ui/core/CardActionArea'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+import Typography from '@material-ui/core/Typography'
+
+const styles = {
+  card: {
+    maxWidth: 345,
+    margin: 10
+  },
+  media: {
+    height: 500
+  }
+}
 
 class App extends Component {
-  state = {
-            value: '',
-            movies: [],
-          }
+  state =
+    {
+      value: '',
+      movies: [],
+    }
   
   onChange = event => {
     this.setState({ value: event.target.value }, () =>
@@ -21,30 +38,45 @@ class App extends Component {
   componentDidMount() {
     axios
       .get('https://api.themoviedb.org/3/movie/top_rated?api_key=59017ce86d5101576f32f47160168519')
-      .then(res => this.setState({ movies: res.data.results }))
-      .catch(err => console.log(err))
+      .then(response => this.setState({ movies: response.data.results }))
+      .catch(error => console.log(error))
   }
 
   render() {
+    const { classes } = this.props
     return (
       <React.Fragment>
         <TextField
           label="Search for the movie!"
-          placeholder="La la land"
+          placeholder="enter movie name"
           fullWidth
           margin="normal"
-          InputLabelProps={{
-            shrink: true
-          }}
+          InputLabelProps={{shrink:true}}
           value={this.state.value}
           onChange={this.onChange}
         />
-        {this.state.movies.map(movie => (
-          <div key={movie.id}>{movie.title}</div>
-        ))}
+        <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around' }}>
+          {this.state.movies.map(movie => (
+            <Card className={classes.card} key={movie.id}>
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                  title="movie poster"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    {movie.title}
+                  </Typography>
+                  <Typography component="p">{movie.overview}</Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          ))}
+        </div>
       </React.Fragment>
     )
   }
 }
 
-export default App
+export default withStyles(styles)(App)
